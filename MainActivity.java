91680610a2ed218a4ffe02c67db50c9d4589ff7a
alinarooi43 <71563311+alinarooi43@users.noreply.github.com>
2020-09-19 21:52:56 +0430

    Add files via upload

---
+++ MainActivity.java
@@ -0,0 +1,18 @@
+package com.NovinTech.Incut;
+
+import android.app.*;
+import android.os.*;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v4.view.WindowCompat;
+import android.view.Window;
+
+public class MainActivity extends AppCompatActivity
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+		
+    }
+}
